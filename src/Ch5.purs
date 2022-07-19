22bd26847f2f4b9a8ad7308a0f76945391f6094a
Jeff Gonzalez <likwid@gmail.com>
2022-07-19 09:51:22 -0500

    Remove extra flip code

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -7,10 +7,7 @@ import Prelude (Unit, show, const)
 flip :: forall a b c. (a -> b -> c) -> b -> a -> c
 flip f x y = f y x
 
-flip' :: forall a b c. (a -> b -> c) -> b -> a -> c
-flip' f = \x y -> f y x
-
 test :: Effect Unit
 test = do
-  log (show (flip' const 20 30))
+  log (show (flip const 10 20))
   
